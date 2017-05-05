2355a31e02aa23b326d144fd080082f78128ba19
Evie Caldwell-Horne <evcaldwell@gmail.com>
2017-05-05 09:52:08 -0700

    wiki title

--- horoscope.rb
+++ horoscope.rb
@@ -5,6 +5,9 @@ require 'oauth'
 require 'sinatra'
 
 get '/' do
-  @prediction = ['🍰', '💔', '💪', '🏆', '🐷', '🍕', '🤖'].sample
+  random_page = HTTParty.get('https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0')
+  @prediction = random_page['query']['random'][0]['title']
+  @id = random_page['query']['random'][0]['id']
   erb :index
 end
+
